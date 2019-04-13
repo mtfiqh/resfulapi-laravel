4cc687750e471611c5c22ae335acede848ef4dc3
Muhammad Taufiq Hidayat <taufiq28101998@gmail.com>
2019-04-13 13:28:29 +0700

    change default string length
    Jika tidak diubah default string nya ketika menjalankan migration akan ada issue, dan tidak bisa menjalankan migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //change the default string length
+        Schema::defaultStringLength(191);
     }
 }
