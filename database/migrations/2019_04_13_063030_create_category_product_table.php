0d1aa5fbb8a514fc50949cacf74f260d1d7cfd95
Muhammad Taufiq Hidayat <taufiq28101998@gmail.com>
2019-04-13 13:48:21 +0700

    Create needed file and the migrations
    create semua file yang dibutuhkan dengan:
    
    php artisan make:model {name} -a
    akan membuat file model, controller, migration, seeder, dan faker
    Lalu, buat file migration sesuai dengan schema

---
+++ database/migrations/2019_04_13_063030_create_category_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->integer('category_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_product');
+    }
+}
---
+++ database/migrations/2019_04_13_063052_create_image_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_product', function (Blueprint $table) {
+            $table->integer('image_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+
+            $table->foreign('image_id')->references('id')->on('images')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_product');
+    }
+}
